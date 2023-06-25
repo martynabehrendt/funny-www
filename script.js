d8d27ea6d04fa714bd8a3537800387ab97f0d1e9
Martyna <martynabehrendt@gmail.com>
2023-06-25 17:13:33 +0200

    ready. done.

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('hey there!')
+
+function showPopup () {
+    var popup = document.getElementById('popup');
+    popup.classList.add('show')
+}
+
+function hidePopup () {
+    var popup = document.getElementById('popup');
+    popup.classList.remove('show')
+}
